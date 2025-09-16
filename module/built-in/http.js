199c29809cae5ca90af1a39d02d858006fde37a9
Abisha Mohanathas <abishamohanathas0431@gmail.com>
2025-09-16 19:49:34 +0530

    http code added

---
+++ module/built-in/http.js
@@ -0,0 +1,18 @@
+//This module, containing both a client and server, can be imported via require('node:http') (CommonJS) or import * as http from 'node:http' (ES module).
+
+//The Node.js HTTP API is very low-level. It deals with stream handling and message parsing only. It parses a message into headers and body but it does not parse the actual headers or the body.
+
+const http = require("node:http");
+
+// Create a local server to receive data from
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "application/json" });
+  res.end(
+    JSON.stringify({
+      data: "Hello World!",
+    })
+  );
+});
+
+server.listen(8000);
+
